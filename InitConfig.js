5263e1faaf63f2ece74f885237d1acd2c7a3eacf
sukitha <sukithaj@gmail.com>
2015-04-01 11:24:50 +0530

    change initial configuration

--- InitConfig.js
+++ InitConfig.js
@@ -25,14 +25,290 @@ var Namespace = jsxml.Namespace,
 ///opt/freeswitch/conf/sip_profiles/external.xml:
 
 
-var internalProfile = format("{0}/{1}", process.env.ConfPath, "/sip_profiles/external.xml" );
-var externalProfile = format("{0}/{1}", process.env.ConfPath, "/sip_profiles/internal.xml" );
+var internalProfile = format("{0}/{1}", process.env.ConfPath, "/sip_profiles/internal.xml" );
+var externalProfile = format("{0}/{1}", process.env.ConfPath, "/sip_profiles/external.xml" );
 var switchpath = format("{0}/{1}", process.env.ConfPath, "/autoload_configs/switch.conf.xml" );
 <!-- <param name="rtp-start-port" value="16384"/> -->
 <!-- <param name="rtp-end-port" value="32768"/> -->
 
 
 
+
+var profilepath = format("{0}/{1}", process.env.ConfPath, "/sip_profiles/" );
+
+
+fs.copy('internal.xml', internalProfile, function (err) {
+    if (err) return console.error(err)
+    console.log("success!")
+
+
+
+    try {
+
+
+        fs.exists(internalProfile, function (exists) {
+
+            if (exists) {
+
+                fs.readFile(internalProfile, "utf-8", function (err, data) {
+
+                    try {
+
+                        var xml = new XML(data);
+                        var child = xml.child("settings");
+
+
+                        var done = false;
+
+                        var descendants = child.descendants('param');
+                        var value = descendants.each(function (obj, index) {
+
+                            if (obj.attribute('name').toXMLString() == 'ext-sip-ip') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+                            if (obj.attribute('name').toXMLString() == 'ext-rtp-ip') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+
+                            if (obj.attribute('name').toXMLString() == 'force-register-domain') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+                            if (obj.attribute('name').toXMLString() == 'force-subscription-domain') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+                            if (obj.attribute('name').toXMLString() == 'force-register-db-domain') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+
+
+
+
+
+
+                            //force-register-domain
+                            //force-subscription-domain
+                            //force-register-db-domain
+                            //apply-inbound-acl
+                            //apply-register-acl
+
+
+
+
+
+
+                        });
+
+
+                        if (!done && process.env.ExternalIP) {
+
+
+                            var externalsip = format("<param name=\"ext-sip-ip\" value=\"{0}\"/>", process.env.ExternalIP);
+
+                            var externalrtp = format("<param name=\"ext-rtp-ip\" value=\"{0}\"/>", process.env.ExternalIP);
+
+                            var xmlexternalsip = new XML(externalsip);
+                            var xmlexternalrtp = new XML(externalrtp);
+
+
+                            child.appendChild(xmlexternalsip);
+                            child.appendChild(xmlexternalrtp);
+                        }
+
+
+                        var xmldata = xml.toXMLString();
+                        console.log(xmldata);
+
+                        fs.outputFile(internalProfile, xmldata, function (err) {
+
+                            if (err) {
+                                console.log(err);
+
+                            } else {
+
+                                console.log("Successfully saved internal");
+                            }
+                        });
+                    } catch (exx) {
+
+                        console.log(exx);
+                    }
+                });
+            }
+            else {
+
+                console.log("Profile is not available ---->");
+            }
+        });
+
+        //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    } catch (exp) {
+
+
+        console.log(exp);
+
+
+    }
+
+
+});
+
+
+fs.copy('external.xml', externalProfile, function (err) {
+    if (err) return console.error(err)
+    console.log("success!")
+
+
+    try {
+
+
+
+        fs.exists(externalProfile, function (exists) {
+
+            if (exists) {
+
+                fs.readFile(externalProfile, "utf-8", function (err, data) {
+
+                    try {
+
+                        var xml = new XML(data);
+                        var child = xml.child("settings");
+
+
+
+
+                        var done = false;
+
+                        var descendants = child.descendants('param');
+                        var value = descendants.each(function (obj, index) {
+
+                            if (obj.attribute('name').toXMLString() == 'ext-sip-ip') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+                            if (obj.attribute('name').toXMLString() == 'ext-rtp-ip') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+
+
+                            if (obj.attribute('name').toXMLString() == 'force-register-domain') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+                            if (obj.attribute('name').toXMLString() == 'force-subscription-domain') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+                            if (obj.attribute('name').toXMLString() == 'force-register-db-domain') {
+                                var val = obj.attribute('value');
+                                if (process.env.ExternalIP) {
+                                    obj.attribute('value').setValue(process.env.ExternalIP);
+                                    done = true;
+                                }
+                            }
+
+
+                        });
+
+
+
+                        if (!done && process.env.ExternalIP) {
+
+
+
+                            var externalsip = format("<param name=\"ext-sip-ip\" value=\"{0}\"/>",process.env.ExternalIP);
+
+                            var externalrtp = format("<param name=\"ext-rtp-ip\" value=\"{0}\"/>",process.env.ExternalIP);
+
+                            var xmlexternalsip = new XML(externalsip);
+                            var xmlexternalrtp = new XML(externalrtp);
+
+
+                            child.appendChild(xmlexternalsip);
+                            child.appendChild(xmlexternalrtp);
+                        }
+
+                        var xmldata = xml.toXMLString();
+                        console.log(xmldata);
+
+                        fs.outputFile(externalProfile, xmldata, function (err) {
+
+                            if (err) {
+                                console.log(err);
+
+                            } else {
+
+                                console.log("Successfully saved internal");
+                            }
+                        });
+                    } catch (exx) {
+
+                        console.log(exx);
+                    }
+                });
+            }
+            else {
+
+                console.log("Profile is not available ---->");
+            }
+        });
+
+        //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    } catch (exp) {
+
+
+        console.log(exp);
+
+
+    }
+
+
+})
 
 
 
@@ -116,261 +392,5 @@ try {
 
 
 
-try {
-
-
-    fs.exists(internalProfile, function (exists) {
-
-        if (exists) {
-
-            fs.readFile(internalProfile, "utf-8", function (err, data) {
-
-                try {
-
-                    var xml = new XML(data);
-                    var child = xml.child("settings");
-
-
-                    var done = false;
-
-                    var descendants = child.descendants('param');
-                    var value = descendants.each(function (obj, index) {
-
-                        if (obj.attribute('name').toXMLString() == 'ext-sip-ip') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-                        if (obj.attribute('name').toXMLString() == 'ext-rtp-ip') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-
-                        if (obj.attribute('name').toXMLString() == 'force-register-domain') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-                        if (obj.attribute('name').toXMLString() == 'force-subscription-domain') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-                        if (obj.attribute('name').toXMLString() == 'force-register-db-domain') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-
-
-
-
-
-
-                        //force-register-domain
-                        //force-subscription-domain
-                        //force-register-db-domain
-                        //apply-inbound-acl
-                        //apply-register-acl
-
-
-
-
-
-
-                    });
-
-
-                    if (!done && process.env.ExternalIP) {
-
-
-                        var externalsip = format("<param name=\"ext-sip-ip\" value=\"{0}\"/>", process.env.ExternalIP);
-
-                        var externalrtp = format("<param name=\"ext-rtp-ip\" value=\"{0}\"/>", process.env.ExternalIP);
-
-                        var xmlexternalsip = new XML(externalsip);
-                        var xmlexternalrtp = new XML(externalrtp);
-
-
-                        child.appendChild(xmlexternalsip);
-                        child.appendChild(xmlexternalrtp);
-                    }
-
-
-                    var xmldata = xml.toXMLString();
-                    console.log(xmldata);
-
-                    fs.outputFile(internalProfile, xmldata, function (err) {
-
-                        if (err) {
-                            console.log(err);
-
-                        } else {
-
-                            console.log("Successfully saved internal");
-                        }
-                    });
-                } catch (exx) {
-
-                    console.log(exx);
-                }
-            });
-        }
-        else {
-
-            console.log("Profile is not available ---->");
-        }
-    });
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-
-} catch (exp) {
-
-
-    console.log(exp);
-
-
-}
-
-
-
-try {
-
-
-
-    fs.exists(externalProfile, function (exists) {
-
-        if (exists) {
-
-            fs.readFile(externalProfile, "utf-8", function (err, data) {
-
-                try {
-
-                    var xml = new XML(data);
-                    var child = xml.child("settings");
-
-
-
-
-                    var done = false;
-
-                    var descendants = child.descendants('param');
-                    var value = descendants.each(function (obj, index) {
-
-                        if (obj.attribute('name').toXMLString() == 'ext-sip-ip') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-                        if (obj.attribute('name').toXMLString() == 'ext-rtp-ip') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-
-
-                        if (obj.attribute('name').toXMLString() == 'force-register-domain') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-                        if (obj.attribute('name').toXMLString() == 'force-subscription-domain') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-                        if (obj.attribute('name').toXMLString() == 'force-register-db-domain') {
-                            var val = obj.attribute('value');
-                            if (process.env.ExternalIP) {
-                                obj.attribute('value').setValue(process.env.ExternalIP);
-                                done = true;
-                            }
-                        }
-
-
-                    });
-
-
-
-                    if (!done && process.env.ExternalIP) {
-
-
-
-                        var externalsip = format("<param name=\"ext-sip-ip\" value=\"{0}\"/>",process.env.ExternalIP);
-
-                        var externalrtp = format("<param name=\"ext-rtp-ip\" value=\"{0}\"/>",process.env.ExternalIP);
-
-                        var xmlexternalsip = new XML(externalsip);
-                        var xmlexternalrtp = new XML(externalrtp);
-
-
-                        child.appendChild(xmlexternalsip);
-                        child.appendChild(xmlexternalrtp);
-                    }
-
-                    var xmldata = xml.toXMLString();
-                    console.log(xmldata);
-
-                    fs.outputFile(externalProfile, xmldata, function (err) {
-
-                        if (err) {
-                            console.log(err);
-
-                        } else {
-
-                            console.log("Successfully saved internal");
-                        }
-                    });
-                } catch (exx) {
-
-                    console.log(exx);
-                }
-            });
-        }
-        else {
-
-            console.log("Profile is not available ---->");
-        }
-    });
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-
-} catch (exp) {
-
-
-    console.log(exp);
-
-
-}
 
 
