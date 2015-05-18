95282154efdaabd6979f78be6a8542d49141073e
achinthau <achintha@duosoftware.com>
2015-05-18 15:30:18 +0530

    change default domain to test user reg

--- scripts/autoloadconfig.sh
+++ scripts/autoloadconfig.sh
@@ -5,5 +5,5 @@ sed -i 's^param name="password" value="ClueCon"^param name="password" value="'$F
 sed -i 's^</settings>^<param name="switchname" value="'$FS_ID'"/></settings>^g' /usr/local/freeswitch/conf/autoload_configs/switch.conf.xml;
 sed -i 's^<param name="auth-realm" value="freeswitch"/>^<!--<param name="auth-realm" value="freeswitch"/>-->^g' /usr/local/freeswitch/conf/autoload_configs/xml_rpc.conf.xml;
 sed -i 's^</bindings>^<binding name="directory"><param name="gateway-url" value="'$SYS_SERVICE_DYN_CON_DIRECTORYPROFILE'" bindings="directory"/></binding> <binding name="callapp"><param name="gateway-url" value="'$SYS_SERVICE_DYN_CON_CALLAPP'" bindings="dialplan" method="get"/></binding></bindings>^g' /usr/local/freeswitch/conf/autoload_configs/xml_curl.conf.xml;
-sed -i 's^<domain name="$${domain}">^<domain name="'$FS_EXTERNALIP'">^g'/usr/local/freeswitch/conf/directory/default.xml
+sed -i 's^<domain name="$${domain}">^<domain name="'$FS_EXTERNALIP'">^g'/usr/local/freeswitch/conf/directory/default.xml;
 
