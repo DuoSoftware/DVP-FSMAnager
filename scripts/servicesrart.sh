a56f0470fb0f0151c348cae506c05ce318ea8d6c
achinthau <achintha@duosoftware.com>
2015-05-19 18:33:25 +0530

    change servicesrart.sh

--- scripts/servicesrart.sh
+++ scripts/servicesrart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nodejs /usr/local/src/fsmanager/InitConfig.js
-#sh /usr/local/src/autoloadconfig.sh
+sh /usr/local/src/autoloadconfig.sh
 sh /usr/local/src/moduleenable.sh
 service freeswitch start
 nodejs /usr/local/src/fsmanager/app.js
